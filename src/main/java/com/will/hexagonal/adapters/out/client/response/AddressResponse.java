3f363a816b27a38055d8458a40b3446e7f1b301c
willianlim <willianrosendodelima@outlook.com>
2023-02-13 08:15:10 -0300

    objeto de resposta do client

---
+++ src/main/java/com/will/hexagonal/adapters/out/client/response/AddressResponse.java
@@ -0,0 +1,13 @@
+package com.will.hexagonal.adapters.out.client.response;
+
+import lombok.Data;
+
+@Data
+public class AddressResponse {
+
+    private String street;
+
+    private String city;
+
+    private String state;
+}
