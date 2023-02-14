24d9d3a00c10cf4497b0bea64fc0eabaf8b6d68c
willianlim <willianrosendodelima@outlook.com>
2023-02-14 08:45:14 -0300

    criacao do CRUD - UPDATE: Criado o controller

--- src/main/java/com/will/hexagonal/adapters/in/controller/CustomerController.java
+++ src/main/java/com/will/hexagonal/adapters/in/controller/CustomerController.java
@@ -3,8 +3,10 @@ package com.will.hexagonal.adapters.in.controller;
 import com.will.hexagonal.adapters.in.controller.mapper.ICustomerMapper;
 import com.will.hexagonal.adapters.in.controller.request.CustomerRequest;
 import com.will.hexagonal.adapters.in.controller.response.CustomerResponse;
+import com.will.hexagonal.application.core.domain.Customer;
 import com.will.hexagonal.application.ports.in.IFindCustomerByIdInputPort;
 import com.will.hexagonal.application.ports.in.IInsertCustomerInputPort;
+import com.will.hexagonal.application.ports.in.IUpdateCustomerInputPort;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,9 @@ public class CustomerController {
 
     @Autowired
     private IFindCustomerByIdInputPort iFindCustomerByIdInputPort;
+
+    @Autowired
+    private IUpdateCustomerInputPort iUpdateCustomerInputPort;
 
     @Autowired
     private ICustomerMapper iCustomerMapper;
@@ -37,5 +42,15 @@ public class CustomerController {
         var customer = iFindCustomerByIdInputPort.find(id);
         var customerResponse = iCustomerMapper.toCustomerResponse(customer);
         return ResponseEntity.ok().body(customerResponse);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable final String id,
+                                       @Valid @RequestBody CustomerRequest customerRequest) {
+
+        Customer customer = iCustomerMapper.toCustomer(customerRequest);
+        customer.setId(id);
+        iUpdateCustomerInputPort.update(customer, customerRequest.getZipCode());
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/will/hexagonal/application/core/usecase/UpdateCustomerUseCase.java
+++ src/main/java/com/will/hexagonal/application/core/usecase/UpdateCustomerUseCase.java
@@ -2,11 +2,12 @@ package com.will.hexagonal.application.core.usecase;
 
 import com.will.hexagonal.application.core.domain.Customer;
 import com.will.hexagonal.application.ports.in.IFindCustomerByIdInputPort;
+import com.will.hexagonal.application.ports.in.IUpdateCustomerInputPort;
 import com.will.hexagonal.application.ports.out.IFindAddressByZipCodeOutputPort;
 import com.will.hexagonal.application.ports.out.IUpdateCustomerOutputPort;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class UpdateCustomerUseCase {
+public class UpdateCustomerUseCase implements IUpdateCustomerInputPort {
 
     @Autowired
     private IFindCustomerByIdInputPort iFindCustomerByIdInputPort;
@@ -17,6 +18,7 @@ public class UpdateCustomerUseCase {
     @Autowired
     private IUpdateCustomerOutputPort iUpdateCustomerOutputPort;
 
+    @Override
     public void update(Customer customer, String zipCode) {
 
         iFindCustomerByIdInputPort.find(customer.getId());
