8a2596f5694eb2f9d909f3ff73f54577d0d206c3
willianlim <willianrosendodelima@outlook.com>
2023-02-16 08:37:42 -0300

    usando o @Autowired ao invez de fazer a injecao manualmente

--- src/main/java/com/will/hexagonal/application/core/usecase/InsertCustomerUseCase.java
+++ src/main/java/com/will/hexagonal/application/core/usecase/InsertCustomerUseCase.java
@@ -4,21 +4,16 @@ import com.will.hexagonal.application.core.domain.Customer;
 import com.will.hexagonal.application.ports.in.IInsertCustomerInputPort;
 import com.will.hexagonal.application.ports.out.IFindAddressByZipCodeOutputPort;
 import com.will.hexagonal.application.ports.out.IInsertCustomerOutputPort;
+import com.will.hexagonal.application.ports.out.ISendCpfForValidationOutputPort;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class InsertCustomerUseCase implements IInsertCustomerInputPort {
 
-    private final IFindAddressByZipCodeOutputPort iFindAddressByZipCodeOutputPort;
+    @Autowired
+    IFindAddressByZipCodeOutputPort iFindAddressByZipCodeOutputPort;
 
-    private final IInsertCustomerOutputPort iInsertCustomerOutputPort;
-
-    public InsertCustomerUseCase(
-            IFindAddressByZipCodeOutputPort iFindAddressByZipCodeOutputPort,
-            IInsertCustomerOutputPort iInsertCustomerOutputPort) {
-
-        this.iFindAddressByZipCodeOutputPort = iFindAddressByZipCodeOutputPort;
-        this.iInsertCustomerOutputPort = iInsertCustomerOutputPort;
-    }
+    @Autowired
+    IInsertCustomerOutputPort iInsertCustomerOutputPort;
 
     public void insert(Customer customer, String zipcode) {
 
