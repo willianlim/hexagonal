ef35212c3601cbc402be955f8f5b93c97501af7e
willianlim <willianrosendodelima@outlook.com>
2023-03-12 14:34:40 -0300

    alterado a forma de como se faz o mapper pois a forma anterior nao funcionava

--- src/main/java/com/will/hexagonal/adapters/out/InsertCustomerAdapter.java
+++ src/main/java/com/will/hexagonal/adapters/out/InsertCustomerAdapter.java
@@ -4,6 +4,7 @@ import com.will.hexagonal.adapters.out.repository.ICustomerRepository;
 import com.will.hexagonal.adapters.out.repository.mapper.ICustomerEntityMapper;
 import com.will.hexagonal.application.core.domain.Customer;
 import com.will.hexagonal.application.ports.out.IInsertCustomerOutputPort;
+import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,12 +14,10 @@ public class InsertCustomerAdapter implements IInsertCustomerOutputPort {
     @Autowired
     private ICustomerRepository iCustomerRepository;
 
-    @Autowired
-    private ICustomerEntityMapper iCustomerEntityMapper;
-
     @Override
     public void inset(Customer customer) {
 
+        ICustomerEntityMapper iCustomerEntityMapper = Mappers.getMapper(ICustomerEntityMapper.class);
         var customerEntity = iCustomerEntityMapper.toCustomerEntity(customer);
         iCustomerRepository.save(customerEntity);
     }
