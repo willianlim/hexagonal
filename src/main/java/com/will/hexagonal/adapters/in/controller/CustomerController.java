8f3906c88febbdba466ffbfc279354b947ba97fa
willianlim <willianrosendodelima@outlook.com>
2023-03-12 14:37:31 -0300

    alterado a forma de como se faz o mapper, a forma anterior nao funcionava

--- src/main/java/com/will/hexagonal/adapters/in/controller/CustomerController.java
+++ src/main/java/com/will/hexagonal/adapters/in/controller/CustomerController.java
@@ -9,6 +9,7 @@ import com.will.hexagonal.application.ports.in.IFindCustomerByIdInputPort;
 import com.will.hexagonal.application.ports.in.IInsertCustomerInputPort;
 import com.will.hexagonal.application.ports.in.IUpdateCustomerInputPort;
 import jakarta.validation.Valid;
+import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,12 +30,10 @@ public class CustomerController {
     @Autowired
     private IDeleteCustomerByIdIputPort iDeleteCustomerByIdIputPort;
 
-    @Autowired
-    private ICustomerMapper iCustomerMapper;
-
     @PostMapping
     public ResponseEntity<Void> insert(@Valid @RequestBody CustomerRequest customerRequest) {
 
+        ICustomerMapper iCustomerMapper = Mappers.getMapper(ICustomerMapper.class);
         var customer = iCustomerMapper.toCustomer(customerRequest);
         iInsertCustomerInputPort.insert(customer, customerRequest.getZipCode());
         return ResponseEntity.ok().build();
@@ -44,6 +43,7 @@ public class CustomerController {
     public ResponseEntity<CustomerResponse> findById(@PathVariable final String id) {
 
         var customer = iFindCustomerByIdInputPort.find(id);
+        ICustomerMapper iCustomerMapper = Mappers.getMapper(ICustomerMapper.class);
         var customerResponse = iCustomerMapper.toCustomerResponse(customer);
         return ResponseEntity.ok().body(customerResponse);
     }
@@ -52,6 +52,7 @@ public class CustomerController {
     public ResponseEntity<Void> update(@PathVariable final String id,
                                        @Valid @RequestBody CustomerRequest customerRequest) {
 
+        ICustomerMapper iCustomerMapper = Mappers.getMapper(ICustomerMapper.class);
         Customer customer = iCustomerMapper.toCustomer(customerRequest);
         customer.setId(id);
         iUpdateCustomerInputPort.update(customer, customerRequest.getZipCode());
